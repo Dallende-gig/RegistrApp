c4f22d165d8a05d4c8b9bacbc32e0dea324053da
Camsito <al.grumiz@gmail.com>
2023-10-15 00:43:23 +0200

    Version 1.0.4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,22 +8,20 @@ import { SQLiteService } from 'src/services/SQLiteService/sqlite.service';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-
   constructor(
     private platform: Platform,
-    private sqliteService: SQLiteService,
+    private sqliteService: SQLiteService
   ) {
     this.initializeApp();
   }
 
-  
   initializeApp() {
     this.platform.ready().then(() => {
       this.sqliteService.createTable().then(() => {
+        this.sqliteService.agregarCredencial('Cam', '12345');
+        this.sqliteService.agregarCredencial('Diego', '123456');
+        this.sqliteService.agregarCredencial('Nicolas', '1234567');
       });
     });
-    this.sqliteService.agregarCredencial('Cam','12345');
-    this.sqliteService.agregarCredencial('Diego','123456');
-    this.sqliteService.agregarCredencial('Nicolas','1234567');
   }
 }
